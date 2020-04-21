30c2fe2f29385ee3e7b25a32d45e361ae21df6c2
Khmel Mykyta <hmelenok@gmail.com>
2020-04-21 16:58:52 +0300

    feat: remove origin

--- src/js/api/wiki.ts
+++ src/js/api/wiki.ts
@@ -3,14 +3,12 @@ import wiki, { Page } from "wikijs";
 export function searchByText(searchTerm: string): Promise<Page> {
   return wiki({
     apiUrl: "https://en.wikipedia.org/w/api.php",
-    origin: window.location.origin,
   }).page(searchTerm);
 }
 
 export function getRandomPage() {
   return wiki({
     apiUrl: "https://en.wikipedia.org/w/api.php",
-    origin: window.location.origin,
   }).random();
 }
 
@@ -18,7 +16,6 @@ export function searchByTitle(title: string): Promise<string> {
   return (
     wiki({
       apiUrl: "https://en.wikipedia.org/w/api.php",
-      origin: window.location.origin,
     })
       .find(title)
       //@ts-ignore
