67824857957126d7b872df41df94e8233f9a9cac
Khmel Mykyta <hmelenok@gmail.com>
2020-04-21 16:52:07 +0300

    feat: add https to requests

--- src/js/api/wiki.ts
+++ src/js/api/wiki.ts
@@ -1,16 +1,25 @@
 import wiki, { Page } from "wikijs";
 
 export function searchByText(searchTerm: string): Promise<Page> {
-  return wiki({ origin: window.location.origin }).page(searchTerm);
+  return wiki({
+    apiUrl: "https://en.wikipedia.org/w/api.php",
+    origin: window.location.origin,
+  }).page(searchTerm);
 }
 
 export function getRandomPage() {
-  return wiki({ origin: window.location.origin }).random();
+  return wiki({
+    apiUrl: "https://en.wikipedia.org/w/api.php",
+    origin: window.location.origin,
+  }).random();
 }
 
 export function searchByTitle(title: string): Promise<string> {
   return (
-    wiki({ origin: window.location.origin })
+    wiki({
+      apiUrl: "https://en.wikipedia.org/w/api.php",
+      origin: window.location.origin,
+    })
       .find(title)
       //@ts-ignore
       .then((page) => page.raw.title)
